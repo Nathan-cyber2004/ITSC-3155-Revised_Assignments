40b27daf8f5a3f5b20350bbbd3e0b44cfc759d1a
Nathan_Pereira <nathanpereira204@gmail.com>
2026-03-27 20:12:04 -0400

    Adding the Sheep Compendium to GitHub

---
+++ tests/test_main.py
@@ -0,0 +1,28 @@
+from fastapi.testclient import TestClient
+from main import app
+client = TestClient(app)
+
+def test_read_sheep():
+    response = client.get(f"/sheep/{1}")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "name": "Spice",
+        "breed": "Gotland",
+        "sex": "ewe"
+    }
+
+def test_add_sheep():
+    response = client.post("/sheep/", json={
+        "id": 99,
+        "name": "Fluffy",
+        "breed": "Merino",
+        "sex": "ewe"
+    })
+    assert response.status_code == 201
+
+    data = response.json()
+    assert data["name"] == "Fluffy"
+    assert data["breed"] == "Merino"
+    assert data["sex"] == "ewe"
+    assert "id" in data
